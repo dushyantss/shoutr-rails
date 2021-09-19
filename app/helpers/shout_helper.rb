ef2ee7aafa7a1bfa64d3f2724e0f8a63113b9f93
Dushyant Singh Shekhawat <contact@dushyantss.com>
2021-09-19 22:59:29 +0530

    Move avatar to it's own helper

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module ShoutHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url
-  end
-
   def like_button(shout)
     if current_user.liked?(shout)
       link_to 'Unlike', unlike_shout_path(shout), method: :delete
