6d8cf85073661eb3d5037f7535c9b9a680e4a7e4
Dushyant Singh Shekhawat <contact@dushyantss.com>
2021-09-19 19:48:03 +0530

    Add photo_shout and active_storage

---
+++ app/models/photo_shout.rb
@@ -0,0 +1,25 @@
+class PhotoShout < ApplicationRecord
+  has_one_attached :image
+
+  validates :image, presence: true
+  validate :correct_image_mime_type
+  validate :image_size_within_range
+
+  def thumb
+    image.variant(resize_to_limit: [200, 200])
+  end
+
+  private
+
+  def correct_image_mime_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png image/gif))
+      errors.add(:image, 'Must be an image')
+    end
+  end
+
+  def image_size_within_range
+    if image.attached? && image.byte_size > 10.megabytes
+      errors.add(:image, 'Must be less than 10 MB')
+    end
+  end
+end
